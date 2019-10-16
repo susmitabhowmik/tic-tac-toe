38c4282d476938847f79731beda0ca61d9de941f
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-10-16 19:58:48 -0400

    enable players to add x and o to the grid

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+var player = 1;
+
 function changeText(id) {
-  
+  if (player === 1) { 
+    document.getElementById(id).innerHTML = "X";
+    document.getElementById(id).disabled = true;
+    player = 2;
+  } else {
+    document.getElementById(id).innerHTML = "O";
+    document.getElementById(id).disabled = true;
+    player = 1;
+  }
 }
